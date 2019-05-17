2abed25038a60b87e179e7767a59e22d017cf2b0
abdullah <abdullah.2010bd@gmail.com>
2019-05-17 15:03:22 +0600

    Nicobits Teechnology Website first initialization

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/abdullah/PycharmProjects/Nicobits_Technology_Website/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
